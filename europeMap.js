90f913e0396a46e539743915a18215a54540b626
vwestric <wsalomon1492@gmx.net>
2020-10-18 17:45:21 +0200

    update base map

--- europeMap.js
+++ europeMap.js
@@ -431,9 +431,10 @@ var map = L.map('mapid').setView([48.856667, 2.351667], 5);
 
 //Traditional
 //Wikimedia
-var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+	attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+	minZoom: 1,
+	maxZoom: 19
 }).addTo(map);
 
 
