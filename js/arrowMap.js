1abe2f4b8610738acafbd4d742b42c23fafa46db
vwestric <wsalomon1492@gmx.net>
2020-11-25 11:15:56 +0100

    update: add base layer

--- js/arrowMap.js
+++ js/arrowMap.js
@@ -16,6 +16,8 @@ var translation = {
             "religious":"édifices religieux",
             "infrastructure":"infrastructure",
             "public":"édifices publiques",
+	    "garden":"jardins",
+	    "geographic":"villes",
             "miscellaneous":"autres"
 
         },
@@ -26,18 +28,24 @@ var translation = {
             "religious":"Religöse Gebäude",
             "infrastructure":"Infrastruktur",
             "public":"Öffentliche Gebäude",
+            "garden":"Gärten",
+	    "geographic":"Städte",
             "miscellaneous":"Sonstige"
         }
     },
     "overlayLabel":{
-        "fra": {
-            "hre": "<b>Saint-Empire 1700</b>",
-        "france": "<b>France 1700</b>"
-        },
-        "de": {
-            "hre": "<b>HRR 1700</b>",
-        "france": "<b>Frankreich 1700</b>"
-        }
+	    "fra": {
+	    	"hre": "<b>Saint-Empire 1700</b>",
+		"france": "<b>France 1700</b>"
+	    },
+	    "de": {
+	    	"hre": "<b>HRR 1700</b>",
+		"france": "<b>Frankreich 1700</b>"
+    	}
+    },
+    "baseLayer": {
+        "fra": "<b>Des endroits</b>",
+        "de": "<b>Orte</b>"
     }
 }
 //Function Definitions
@@ -81,6 +89,7 @@ function overlayNames(lang) {
           "Pitzler":"",
           "Neumann":"",
           "Harrach":"",
+          "<b>Des endroits</b>":"",
           "<b>France 1700</b>":"",
           "<b>Saint-Empire 1700</b>":""    
         };
@@ -93,6 +102,7 @@ function overlayNames(lang) {
           "Pitzler":"",
           "Neumann":"",
           "Harrach":"",
+          "<b>Orte</b>":"",
           "<b>HRR 1700</b>":"",
           "<b>Frankreich 1700</b>":""    
         };
@@ -134,20 +144,22 @@ var hrrStyle = {
 //Init Map
 var map = L.map('mapid').setView([48.856667, 2.351667], 5);
 
-//Traditional
-//Wikimedia
-var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
-  attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-  minZoom: 1,
-  maxZoom: 19
+//Minimal
+//Carto DB No Labels
+var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>. Icons: <a href="https://thenounproject.com/">The Noun Project</a>',
+	subdomains: 'abcd',
+	minZoom:6,
+	maxZoom: 19
 });
 
-//Minimal
+
+//Traditional
 //Carto DB
 var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>. Icons: <a href="https://thenounproject.com/">The Noun Project</a>',
     subdomains: 'abcd',
-        minZoom: 1,
+    minZoom:6,
     maxZoom: 19
 }).addTo(map);
 
@@ -156,10 +168,10 @@ var franceLabel = overlayName("france", lang);
 var hreLabel = overlayName("hrr", lang);
 
 //Layer Groups
-//Base Maps
+//Base Maps Container
 var baseMaps = {
-    "Wikimedia": Wikimedia,
-    "Carto DB" : CartoDB_Positron
+    "Carto DB" : CartoDB_Positron,
+    "Carto DB No Labels": CartoDB_PositronNoLabels
 };
 
 //Overlay Maps
@@ -197,25 +209,65 @@ d === "Harrach" ? '#ff0000' :
 'black';
 }
 
+//Get Icon For Legend
+function getIcon(d) {
+return d === "édifices domestiques" ? 'https://vwestric.github.io/paris-project/svg/house.svg' :
+d === "constructions militaires" ? 'https://vwestric.github.io/paris-project/svg/military.svg' :
+d === "édifices religieux" ? 'https://vwestric.github.io/paris-project/svg/spiritual.svg' :
+d === "infrastructure" ? 'https://vwestric.github.io/paris-project/svg/bridge.svg' :
+d === "édifices publiques" ? 'https://vwestric.github.io/paris-project/svg/public.svg' :
+d === "jardins" ? 'https://vwestric.github.io/paris-project/svg/garden.svg' :
+d === "villes" ? 'https://vwestric.github.io/paris-project/svg/marker.svg' :
+d === "autres" ? 'https://vwestric.github.io/paris-project/svg/circle.svg' :
+d === "Wohngebäude" ? 'https://vwestric.github.io/paris-project/svg/house.svg' :
+d === "Militärische Gebäude" ? 'https://vwestric.github.io/paris-project/svg/military.svg' :
+d === "Religöse Gebäude" ? 'https://vwestric.github.io/paris-project/svg/spiritual.svg' :
+d === "Infrastruktur" ? 'https://vwestric.github.io/paris-project/svg/bridge.svg' :
+d === "Öffentliche Gebäude" ? 'https://vwestric.github.io/paris-project/svg/public.svg' :
+d === "Gärten" ? 'https://vwestric.github.io/paris-project/svg/garden.svg' :
+d === "Städte" ? 'https://vwestric.github.io/paris-project/svg/marker.svg' :
+d === "Sonstige" ? 'https://vwestric.github.io/paris-project/svg/circle.svg' :
+'https://vwestric.github.io/paris-project/svg/circle.svg';
+}
+
+
+
 var legend = L.control({position: 'bottomleft'});
     legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
     labels = ['<strong>'+ translation["legendLabel"][lang] +'</strong>'],
     symbols = ['<strong>'+ translation["symbolsLabel"][lang]["Symbols"]+'</strong>'],
-    travelers = ["Pitzler","Harrach","Corfey","Knesebeck","Neumann"];
+    travelers = ["Sturm","Knesebeck","Corfey","Pitzler","Neumann","Harrach"];
+    icons = [
+             translation["symbolsLabel"][lang]["domestic"],
+             translation["symbolsLabel"][lang]["military"],
+             translation["symbolsLabel"][lang]["religious"],
+             translation["symbolsLabel"][lang]["infrastructure"],
+             translation["symbolsLabel"][lang]["public"],
+	     translation["symbolsLabel"][lang]["garden"],
+	     translation["symbolsLabel"][lang]["geographic"],
+             translation["symbolsLabel"][lang]["miscellaneous"],
+             ]
 
     for (var i = 0; i < travelers.length; i++) {
-    div.innerHTML += 
-        labels.push(
-            '<i style="background:' + getColor(travelers[i]) + '""></i> ' + travelers[i]);
-    }
+	div.innerHTML += 
+		labels.push(
+			'<i style="background:' + getColor(travelers[i]) + '""></i> ' + travelers[i]);
+	}
 
-    div.innerHTML = labels.join('<br>');
+	for (var i = 0; i < icons.length; i++) {
+	div.innerHTML += 
+		symbols.push(
+			'<img src="' + getIcon(icons[i]) + '" width=17 height=17> ' + icons[i]);
+	}
 
-    return div;
-    };
+	div.innerHTML = labels.join('<br>') + '<br>' + symbols.join('<br>');
+
+	return div;
+	};
     legend.addTo(map);
+
 
 //Fetch Data
 //In the actual Map, Data would be Fetched via this Request
@@ -259,6 +311,22 @@ overlayMaps["Harrach"] = harrach
 
 //Add layer control to map
 L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+
+});
+
+//All Places
+fetch('https://vwestric.github.io/paris-project/geojson/visitsEuropeGeolocated.geojson')
+  .then(response => response.json())
+  .then(data => {
+    //Base Group
+    var all = L.layerGroup([L.geoJSON(data, {onEachFeature: onEachFeature, filter:function(feature, layer) {
+        return feature.properties.visitor=="All";
+    }})]).addTo(map);
+    
+    overlayMaps[translation["baseLayer"][lang]] = all
+    //Add layer control to map
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 
 });
